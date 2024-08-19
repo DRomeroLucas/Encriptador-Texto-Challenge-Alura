e9a958e19cfed5d793d28669146ba327ba9a1f8a
DR Dev <dromeroldev@gmail.com>
2024-08-19 15:02:40 -0500

    Se agrega modo oscuro a la web, de manera general al elemento del elemento html y a alguinos contenedores de forma especifica

--- scripts/app.js
+++ scripts/app.js
@@ -187,3 +187,14 @@ function desencriptar() {
         };
     };
 }
+
+// Activar o desactivar modo oscuro
+function cambiarTema() {
+    let htmlElement = document.documentElement;
+
+    if (htmlElement.getAttribute('data-theme') === 'dark') {
+        htmlElement.removeAttribute('data-theme');
+    } else {
+        htmlElement.setAttribute('data-theme', 'dark');
+    }    
+};
